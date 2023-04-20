4db385532c865b0be0ea7d70756d5bd305191655
world324digital <world.digital.dev@gmail.com>
2023-04-20 15:14:56 +0300

    remove local notification part in ios

--- ios/Runner/AppDelegate.swift
+++ ios/Runner/AppDelegate.swift
@@ -1,6 +1,5 @@
 import UIKit
 import Flutter
-import flutter_local_notifications
 import workmanager
 
 @UIApplicationMain
@@ -9,15 +8,6 @@ import workmanager
     _ application: UIApplication,
     didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?
   ) -> Bool {
-    // This is required to make any communication available in the action isolate.
-    FlutterLocalNotificationsPlugin.setPluginRegistrantCallback { (registry) in
-        GeneratedPluginRegistrant.register(with: registry)
-    }
-
-    if #available(iOS 10.0, *) {
-      UNUserNotificationCenter.current().delegate = self as UNUserNotificationCenterDelegate
-    }
-
     GeneratedPluginRegistrant.register(with: self)
   
     UIApplication.shared.setMinimumBackgroundFetchInterval(TimeInterval(60)) 
